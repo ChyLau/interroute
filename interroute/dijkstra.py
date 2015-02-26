88a59a6265e0f5eb70ef4b17708cb4e99d4282e6
Chy Lau <clau@unicorn.lan>
2015-02-26 22:24:48 +0100

    Copied algorithm and map from internet. Maybe implement it into a GUI...next time.

--- interroute/dijkstra.py
+++ interroute/dijkstra.py
@@ -1,22 +1,70 @@
-def dijkstra(graph, source):
-    dist[source] = 0
-    prev[source] = undef
+#def dijkstra(graph, source):
+#    dist[source] = 0
+#    prev[source] = undef
+#
+#    for vertex in graph:
+#        if vertex != source:
+#            dist[vertex] = float("inf")
+#            prev[vertex] = None
+#
+#        q.append(vertex)
+#
+#    while len(q) != 0:
+#        u = min_dist
+#        q.remove(u)
+#
+#        for neighbor of u:
+#            alt = dist[u] + length(u, v)
+#            if alt < dist[neighbor]:
+#                dist[neighbor] = alt
+#                prev[neighbor] = u
+#
+#    return dist[], prev[]
 
-    for vertex in graph:
-        if vertex != source:
-            dist[vertex] = float("inf")
-            prev[vertex] = None
+def dijkstra(net, s, t):
+    # sanity check
+    if s == t:
+        print "The start and terminal nodes are the same. Minimum distance is 0."
+    if net.has_key(s)==False:
+        print "There is no start node called " + str(s) + "."
+    if net.has_key(t)==False:
+        print "There is no terminal node called " + str(t) + "."
+    # create a labels dictionary
+    labels={}
+    # record whether a label was updated
+    order={}
+    # populate an initial labels dictionary
+    for i in net.keys():
+        if i == s: labels[i] = 0 # shortest distance form s to s is 0
+        else: labels[i] = float("inf") # initial labels are infinity
+    from copy import copy
+    drop1 = copy(labels) # used for looping
+    ## begin algorithm
+    while len(drop1) > 0:
+        # find the key with the lowest label
+        minNode = min(drop1, key = drop1.get) #minNode is the node with the smallest label
+        # update labels for nodes that are connected to minNode
+        for i in net[minNode]:
+            if labels[i] > (labels[minNode] + net[minNode][i]):
+                labels[i] = labels[minNode] + net[minNode][i]
+                drop1[i] = labels[minNode] + net[minNode][i]
+                order[i] = minNode
+        del drop1[minNode] # once a node has been visited, it's excluded from drop1
+    ## end algorithm
+    # print shortest path
+    temp = copy(t)
+    rpath = []
+    path = []
+    while 1:
+        rpath.append(temp)
+        if order.has_key(temp): temp = order[temp]
+        else: print "There is no path from " + str(s) + " to " + str(t) + "."
+        if temp == s:
+            rpath.append(temp)
+            break
+    for j in range(len(rpath)-1,-1,-1):
+        path.append(rpath[j])
+    print "The shortest path from " + s + " to " + t + " is " + str(path) + ". Minimum distance is " + str(labels[t]) + "."
 
-        q.append(vertex)
-
-    while len(q) != 0:
-        u = min_dist
-        q.remove(u)
-
-        for neighbor of u:
-            alt = dist[u] + length(u, v)
-            if alt < dist[neighbor]:
-                dist[neighbor] = alt
-                prev[neighbor] = u
-
-    return dist[], prev[]
+# Given a large random network find the shortest path from '0' to '5'
+#print dijkstra(net=randNet(), s='0', t='5')
--- interroute/main.py
+++ interroute/main.py
@@ -1,18 +1,28 @@
-import networkx as nx
 import matplotlib.pyplot as plt
+import dijkstra
+
+def main():
+	#def drawMap():
+	#    graph = nx.Graph()
+	#
+	#    #graph.add_node(10)
+	#    #graph.add_nodes_from([2,3])
+	#
+	#    graph.add_edges_from([(1,2),(1,3)])
+	#
+	#    nx.draw(graph)
+	#    plt.show()
+	
+	net = {'0':{'1':100, '2':300},
+	       '1':{'3':500, '4':500, '5':100},
+	       '2':{'4':100, '5':100},
+	       '3':{'5':20},
+	       '4':{'5':20},
+	       '5':{}
+	       }
+	
+	dijkstra.dijkstra(net, '0', '5')
 
 
-def drawMap():
-    graph = nx.Graph()
-
-    #graph.add_node(10)
-    #graph.add_nodes_from([2,3])
-
-    graph.add_edges_from([(1,2),(1,3)])
-
-    nx.draw(graph)
-    plt.show()
-
-
-if __name__ == "__main__":
-    drawMap()
+if __name__ == "__main__":                                                     
+	main()
